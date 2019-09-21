1ddf9500ba981444886734290057bce3cb83a92e
GregsAStar <gregmitchell0@protonmail.com>
2019-09-21 16:52:29 -0400

    Changed to Gloss

--- src/Game.hs
+++ src/Game.hs
@@ -2,31 +2,38 @@ module Game
     ( beginGame
     ) where
 
-import Control.Monad
-
-import Data.Text hiding (map)
-import Foreign.C.Types
-
-import SDL
-import SDL.Vect
---import Paths_sdl(getDataFileName)
-
-
-screenWidth, screenHeight :: CInt
-(screenWidth, screenHeight) = (640, 480)
-
-screenSize = V2 screenWidth screenHeight
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
 
 beginGame :: IO ()
 beginGame = do
-    initializeAll
-    HintRenderScaleQuality $= ScaleLinear
-    window <- createWindow (pack "Eye Spy") defaultWindow {windowInitialSize = screenSize}
-    renderer <- createRenderer window (-1) defaultRenderer
-    gameLoop renderer
+    let fps = 60
+    let window = InWindow "Eye Spy" (640, 480) (0, 0)
+    initialWorld <- genInitialWorld
+    playIO window black fps initialWorld draw eventHandler (\_ -> return . id)
+    
 
-gameLoop :: Renderer -> IO ()
-gameLoop renderer = do
-    events <- pollEvents
+type Riddle = [(String, Bool)] --[(Riddle_Word, Found_State)]
 
-    unless (QuitEvent `elem` (map eventPayload events)) (gameLoop renderer)
+data GameState = Menu [String] | Board Picture Picture Riddle --Main_Image Text_Background Riddle
+
+
+genInitialWorld :: IO GameState
+genInitialWorld = do
+    mainImageBMP <- loadBMP "./assets/placeholder.bmp" --For test purposes, will be changed later
+    textBgBMP <- loadBMP "./assets/placeholder.bmp" --For test purposes, will be changed later
+    let mainImage = scale 1.0 0.5 mainImageBMP
+    let textBg = translate 0 240 $ scale 1.0 0.5 textBgBMP
+    let riddle = [("Item 1", False), ("Item 2", False), ("Item 3", False)] --For test purposes, will be changed later
+    return $ Board mainImage textBg riddle
+
+draw :: GameState -> IO Picture
+draw state
+    |Menu options <- state
+    = return Blank
+    |Board mainImage textBG riddle <- state
+    = do
+        return $ pictures (mainImage:textBG:[])
+
+eventHandler :: Event -> GameState -> IO GameState
+eventHandler _ state = return state
